14f00592b505550d268b7fcc432cbc66683c6e1e
ichaudry <ibraheem27496@gmail.com>
2020-03-18 22:56:52 -0700

    Fixed major bugs with tokenizing across multiple threads. Added strtok_r for thread safe operations.Output is close to expected results.

--- src/main.c
+++ src/main.c
@@ -45,11 +45,11 @@ int main(int argc, char ** argv){
     struct timespec startTime;
     struct timespec endTime;
     clock_gettime(CLOCK_REALTIME, &startTime);
-//
-//    if (pthread_mutex_init(&hashTable_mutex, NULL) != 0) {
-//        printf("\n mutex init has failed\n");
-//        return 1;
-//    }
+
+    if (pthread_mutex_init(&hashTable_mutex, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
 
     //Number of threads
     int nThreads;
@@ -62,7 +62,7 @@ int main(int argc, char ** argv){
     FILE * filePointer;
 
     //Hash Table that stores final frequencies
-//    ht= ht_create();
+    ht= ht_create();
 //    ht_set(ht,"hello world","1");
 //    ht_dump(ht);
 
@@ -131,18 +131,28 @@ int main(int argc, char ** argv){
         //catch error by checking if rc is equal to zero
         rc = pthread_join(threads[i], NULL);
         if(rc!=0){
-            perror("Error joining thread");
+            printf("Error joining thread %d\n",i);
             printf("Error code: %d\n",rc);
         }
     }
 
-//    pthread_mutex_destroy(&hashTable_mutex);
+    pthread_mutex_destroy(&hashTable_mutex);
 
-//    ht_dump(ht);
-//    printf("The frequency of Pierre is : %s\n", ht_get(ht,"prince"));
+
+
+
+
+
+
+
+
+
+
+
+    printf("The frequency of Pierre is : %s\n", ht_get(ht,"prince"));
 
     //To-do check if the hashtable has been successfully freed
-//    ht_free(ht);
+    ht_free(ht);
 
 
 
@@ -183,13 +193,13 @@ void *processFile(void *arguments)
     int index=0;
 
 
-    printf("Start: %lu\n",start);
-    printf("End: %lu\n",end);
+//    printf("Start: %lu\n",start);
+//    printf("End: %lu\n",end);
 
     //Read file to buffer
     char * fileContent= readFile(filePointer,start,end);
 
-    printf("This is the size of file buffer: %lu\n",strlen(fileContent));
+//    printf("This is the size of file buffer: %lu\n",strlen(fileContent));
 
     //Tokenize file contents
     char **tokens=tokenizeFileContents(fileContent);
@@ -199,20 +209,20 @@ void *processFile(void *arguments)
         if(tokens[index]==NULL){
             break;
         }
-//        char * token=tokens[index];
-//
-//        pthread_mutex_lock(&hashTable_mutex);
-//        if(ht_get(ht,token)==NULL){
-//            ht_set(ht,token,"1");
-//        }
-//        else{
-//            int count=atoi(ht_get(ht,token))+1;
-//            char  *buf=malloc(32);
-//            snprintf(buf, sizeof(buf), "%d", count);
-//            ht_set(ht,token,buf);
-//            free(buf);
-//        }
-//        pthread_mutex_unlock(&hashTable_mutex);
+        char * token=tokens[index];
+
+        pthread_mutex_lock(&hashTable_mutex);
+        if(ht_get(ht,token)==NULL){
+            ht_set(ht,token,"1");
+        }
+        else{
+            int count=atoi(ht_get(ht,token))+1;
+            char  *buf=malloc(32);
+            snprintf(buf, sizeof(buf), "%d", count);
+            ht_set(ht,token,buf);
+            free(buf);
+        }
+        pthread_mutex_unlock(&hashTable_mutex);
 
         index++;
     }
@@ -222,7 +232,6 @@ void *processFile(void *arguments)
 
     //Free the file content buffer
     free(fileContent);
-//    fileContent=NULL;
     free(tokens);
 
 
@@ -246,7 +255,7 @@ char * readFile(void * filePointer,long start, long end){
     char * fileContent = 0;
     filePointer = fopen (filePath, "r");
     long chunkSize= end-start;
-    printf("This is the chunk size calculated by the readFile func: %ld\n",chunkSize);
+//    printf("This is the chunk size calculated by the readFile func: %ld\n",chunkSize);
 
     if (filePointer)
     {
@@ -267,7 +276,7 @@ char * readFile(void * filePointer,long start, long end){
 
     if (fileContent)
     {
-        printf("The buffer was correctly allocated using what was in the text file\n");
+//        printf("The buffer was correctly allocated using what was in the text file\n");
     }
 
     return fileContent;
@@ -279,18 +288,19 @@ char **tokenizeFileContents(char *fileContent)
     int bufferSize = TOKENIZER_BUFFER_SIZE, position = 0;
     char **tokens = malloc(bufferSize * sizeof(char*));
     char *token;
+    char *savePtr;
 
     if (!tokens) {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(fileContent, TOKENIZER_DELIMITER);
+    token = strtok_r(fileContent, TOKENIZER_DELIMITER,&savePtr);
     while (token != NULL) {
 //        printf("The token is: %s and the string length is: %lu\n",token,strlen(token));
 
         if(strlen(token)>5){
-//            printf("The token is : %s and the string length is : %d\n",token,strlen(token));
+//            printf("The token is : %s and the string length is : %lu\n",token,strlen(token));
             //Covert the string to lowercase
             for(int i = 0; i< strlen(token); i++){
                 token[i] = tolower(token[i]);
@@ -309,7 +319,7 @@ char **tokenizeFileContents(char *fileContent)
             }
         }
 
-        token = strtok(NULL, TOKENIZER_DELIMITER);
+        token = strtok_r(NULL, TOKENIZER_DELIMITER,&savePtr);
     }
 
     tokens[position] = NULL;
